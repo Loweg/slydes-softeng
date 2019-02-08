544c7697810cfc8ee91b94954c0d80b170e40754
longarms07 <ecridd@gmail.com>
2019-02-08 14:25:19 -0500

    Margins
    Added margins, started building groundwork for images

--- Scene.py
+++ Scene.py
@@ -9,28 +9,73 @@ class Slide:
 	def __init__(self):
 		#Initializes the basic slide. Handled as a GTK text view
 		self.box = Gtk.Grid()
+		self.scrolledwindow = Gtk.ScrolledWindow()
+		self.scrolledwindow.set_hexpand(False)
+		self.scrolledwindow.set_vexpand(False)
+		self.box.add(self.scrolledwindow)
 		self.textview = Gtk.TextView()
-		self.textview.set_size_request(960,540) #16:9 aspect ratio. Can be changed as needed
+		self.height = 540
+		self.width = 910
+		self.scrolledwindow.set_size_request(self.width, self.height)
+		self.textview.set_size_request(self.width, self.height) #16:9 aspect ratio. Can be changed as needed
 		self.textbuffer = self.textview.get_buffer()
 		self.pixbuflist = [] #image list
 
 
 		#Sets our default text and font
-		self.currentfont = ""
-		self.set_font("sans", "11")
+		self.currentfont = "sans"
+		self.currentfontsize = "11"
+		self.set_font(self.currentfont, self.currentfontsize)
+		
+		#set top and bottom margins. Note: Right/Left aren't compatible with text wrapping
+		self.textview.set_top_margin(25)
+		self.textview.set_bottom_margin(25)
+		#Handle right/left margins as non-editible textboxes
+		self.rmsize = 25
+		self.lmsize = 25
+		self.rightmargin = Gtk.TextView()
+		self.rightmargin.set_editable(False)
+		self.rightmargin.set_size_request(25, self.height)
+		self.box.attach_next_to(self.rightmargin, self.scrolledwindow, 1,1,1)
+
+		self.leftmargin = Gtk.TextView()
+		self.leftmargin.set_editable(False)
+		self.leftmargin.set_size_request(25, self.height)
+		self.box.attach_next_to(self.leftmargin, self.scrolledwindow, 0, 1, 1)
+
+
 
 		self.textbuffer.set_text("You can type here. Select and delete this text to remove.")
 		#Allows wraparound. Character wraparound was chosen over word wraparound to prevent
+		self.textview.set_wrap_mode(Gtk.WrapMode.CHAR)
 		#Long words from increasing slide size
-		self.box.add(self.textview) 
+		self.scrolledwindow.add(self.textview) 
+		#self.box.show()
+
 
 
 	def resize_slide(self, width, height, pangoFont):
 		#Resizes the slide and the textbox as a result.
-		self.textview.set_size_request(width, height)
+		self.textview.set_size_request(width-(self.lmsize+self.rmsize), height)
+		self.leftmargin.set_size_request(self.lmsize, height)
+		self.rightmargin.set_size_request(self.rmsize, height)
 		#Pango requires a string w/font and size. Example: "Sans 20"
 		self.textview.modify_font(Pango.FontDescription(pangoFont))
 		#Stubbed, needs to find a way to resize images once they are added.
+
+
+
+	def margin_size(margin, marginsize):
+		if margin == "left":
+			self.leftmargin.set_size_request(marginsize, self.height)
+			self.lmsize = marginsize
+		elif margin == "right":
+			self.rightmargin.set_size_request(marginsize, self.height)
+			self.rmsize == marginsize
+		elif margin == "top":
+			self.textview.set_top_margin(marginsize)
+		elif margin == "top":
+			self.textview.set_bottom_margin(marginsize)
 
 
 
@@ -52,8 +97,27 @@ class Slide:
 			print("Warning! This font may be too small to read!")
 			#stubbed, do something to show warning to end user
 		newfont = font+" "+str(size)
-		self.currentfont = newfont
+		self.currentfont = font
+		self.currentfontsize = size
 		self.textview.modify_font(Pango.FontDescription(self.currentfont))
+
+
+
+	def get_height(self):
+		return self.height
+
+
+
+	def get_width(self):
+		return self.width
+
+
+
+	def set_dimensions(self, newwidth, newheight):
+		#set the width and height of the slide
+		self.width = newwidth
+		self.height = newheight
+		self.textview.set_size_request(width, height)
 
 
 
@@ -67,7 +131,9 @@ class Slide:
 		self.textbuffer.insert_pixbuf(iterator ,image)
 		#untested
 
-	def on_button_clicked(self, tag):
+
+
+	def tag_button_clicked(self, tag):
 		#This function allows us to do something when a button is pressed
 		#The button's tag will tell us what to do, these will be coded into the buttons
 		bounds = self.textbuffer.get_selection_bounds #selected text
@@ -76,7 +142,21 @@ class Slide:
 			self.textbuffer.apply_tag(tag, start, end)
 
 
+	def thumbnail(self):
+		#Resize slide and everything on it to the thumbnail size
+		one=1
+		#stubbed
 
+	def fullscreen(self):
+		#Resize slide and all its elements to fit the fullscreen
+		one=1
+		#stubbed
+	
+
+
+class SlideDeck:
+	def __init__(self):
+		self.deck = []
 
 
 
@@ -87,11 +167,16 @@ def slideTest():
 	win = Gtk.Window()
 	win.set_default_size(1000,1000)
 	win.connect("destroy", Gtk.main_quit)
+	
 	slide = Slide()
 	win.add(slide.get_slide())
 	#slide.resize_slide(1820,1080, "sans 42") #Successful
 	#slide.font_size("sans", 40) #Successful
 	win.show_all()
+	
 	Gtk.main()
+	
+
+
 
 slideTest()
