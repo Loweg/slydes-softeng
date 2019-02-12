9f84420c24c127b526d5ac4c33884ef8f3b095e7
longarms07 <ecridd@gmail.com>
2019-02-12 14:40:33 -0500

    Latest version, have slide deck stuff

--- Scene.py
+++ Scene.py
@@ -25,7 +25,7 @@ class Slide:
 		#Sets our default text and font
 		self.currentfont = "sans"
 		self.currentfontsize = "11"
-		self.set_font(self.currentfont, self.currentfontsize)
+		self.set_font(self.currentfont, self.currentfontsize, False)
 		
 		#set top and bottom margins. Note: Right/Left aren't compatible with text wrapping
 		self.textview.set_top_margin(25)
@@ -60,7 +60,8 @@ class Slide:
 		self.leftmargin.set_size_request(self.lmsize, height)
 		self.rightmargin.set_size_request(self.rmsize, height)
 		#Pango requires a string w/font and size. Example: "Sans 20"
-		self.textview.modify_font(Pango.FontDescription(pangoFont))
+		self.textview.modify_font(Pango.FontDescription(pangoFont)) #add scrollbar as well
+
 		#Stubbed, needs to find a way to resize images once they are added.
 
 
@@ -91,15 +92,22 @@ class Slide:
 
 
 
-	def set_font(self, font, size):
+	def set_font(self, font, size, warn):
 		#take string font and integer size to resize the fonts
-		if(int(size) < 11):
+		if(int(size) < 11 and warn == True):
 			print("Warning! This font may be too small to read!")
 			#stubbed, do something to show warning to end user
 		newfont = font+" "+str(size)
 		self.currentfont = font
 		self.currentfontsize = size
 		self.textview.modify_font(Pango.FontDescription(newfont))
+
+
+
+	def set_font_at(self, font, size):
+		#Change only the font in the currently selected area
+		two==2
+		#stubbed
 
 
 
@@ -117,8 +125,28 @@ class Slide:
 		#set the width and height of the slide
 		self.width = newwidth-(self.lmsize+self.rmsize)
 		self.height = newheight
+		self.set_font(self.currentfont, 2, False)
+		self.leftmargin.set_size_request(self.lmsize, self.height)
+		self.rightmargin.set_size_request(self.rmsize, self.height)
+		self.scrolledwindow.set_size_request(self.width, self.height)
 		self.textview.set_size_request(self.width, self.height)
+		
 
+	def insert_image_clicked(self,button):
+		dialog = Gtk.FileChooserDialog ("Open Image", button.get_toplevel(), Gtk.FIleChooserAction.OPEN)
+		dialog.add_button(Gtk.STOCK_CANCEL, 0)
+		dialog.add_button(Gtk.STOCK_OK, 1)
+		dialog.set_default_response(1)
+
+		filefilter = Gtk.FileFilter()
+		filefilter.add_pixbuf_formats()
+		dialog.set_filter(filefilter)
+
+		if dialog.run() == 1:
+			pic = GdkPixbuf.Pixbuf.new_from_file(dialog.get_filename())
+			self.insert_image(pic)
+
+		dialog.destroy()
 
 
 	def insert_image(self, image):
@@ -139,7 +167,6 @@ class Slide:
 		#scale all images to the new dimensions
 		index = 0
 		for image in self.images:
-			print("here")
 			image = image.scale_simple(newwidth, newheight, 2)
 			mark = self.imagemarks[0]
 			iterator = self.textbuffer.get_iter_at_mark(mark)
@@ -147,7 +174,7 @@ class Slide:
 			self.textbuffer.insert_pixbuf(iterator ,image)
 
 
-	def tag_button_clicked(self, tag):
+	def tag_button_clicked(self, button, tag):
 		#This function allows us to do something when a button is pressed
 		#The button's tag will tell us what to do, these will be coded into the buttons
 		bounds = self.textbuffer.get_selection_bounds #selected text
@@ -158,7 +185,7 @@ class Slide:
 
 	def thumbnail(self):
 		#Resize slide and everything on it to the thumbnail size
-		one=1
+		self.set_dimensions(100, 100)
 		#stubbed
 
 
@@ -166,15 +193,29 @@ class Slide:
 		#Resize slide and all its elements to fit the fullscreen
 		one=1
 		#stubbed
-	
+
+
+
 
 
 class SlideDeck:
 	def __init__(self):
 		self.deck = []
+		self.buttons = []
 		firstslide = Slide()
 		self.deck.append(firstslide)
 		self.currentslide = firstslide
+		self.grid = Gtk.Grid()
+		self.list = Gtk.ListBox()
+		self.side = Gtk.ScrolledWindow()
+		self.side.set_size_request(100,540)
+		self.side.add(self.list)
+		self.grid.add(self.side)
+		self.grid.add(firstslide.get_slide())
+		firstbutton = Gtk.Button(label = "1")
+		firstbutton.connect("clicked", self.on_button_clicked)
+		self.list.add(firstbutton)
+		self.buttons.append(firstbutton)
 
 
 
@@ -182,12 +223,29 @@ class SlideDeck:
 		#Returns the current slide
 		return self.currentslide
 
+	def on_button_clicked(self, button):
+		slide_num = int(button.get_label())
+		self.set_current_slide(slide_num)
+
+	def set_current_slide(self, slide_number):
+		self.grid.remove(self.currentslide.get_slide())
+		self.list.show()
+		self.currentslide = self.retrieve_slide(slide_number)
+		self.grid.add(self.currentslide.get_slide())
+		self.currentslide.get_slide().show()
+		self.grid.show_all()
+
 
 	def make_new_slide(self):
 		#Make a new slide then return it
 		newslide = Slide()
-		newslide.set_font("sans", "25") #used to test that making a new slide works
+		#newslide.set_font("sans", "25") #used to test that making a new slide works
 		self.deck.append(newslide)
+		newindex = len(self.deck)
+		newbutton = Gtk.Button(label = newindex)
+		newbutton.connect("clicked", self.on_button_clicked)
+		self.list.add(newbutton)
+		self.buttons.append(newbutton)
 		return newslide
 
 
@@ -205,6 +263,10 @@ class SlideDeck:
 				index = index+1
 		return currentslide
 
+	def get_view(self):
+		return self.grid
+
+
 
 
 
@@ -216,11 +278,10 @@ def slideTest():
 	
 	slidedeck = SlideDeck()
 	slide = slidedeck.get_current_slide()
-	win.add(slide.get_slide())
-	win.remove(slide.get_slide())
+	win.add(slidedeck.get_view())
 	slidedeck.make_new_slide()
 	slide = slidedeck.retrieve_slide(2)
-	win.add(slide.get_slide())
+	slidedeck.set_current_slide(2)
 	#slide.resize_slide(1820,1080, "sans 42") #Successful
 	#slide.set_font("sans", 20) #Successful
 	pic = GdkPixbuf.Pixbuf.new_from_file("apple-touch-icon-144x144-precomposed.png") #use GdkPixbuf to load images.
